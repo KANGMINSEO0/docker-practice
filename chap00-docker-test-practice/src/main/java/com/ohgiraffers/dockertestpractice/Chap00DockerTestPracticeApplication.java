52e25df660cb734be6fd7adaff6193aedbc3ce68
KANGMINSEO0 <juhee0629032@gmail.com>
2024-04-24 20:43:32 +0900

    chap00-docker-test-practice

---
+++ chap00-docker-test-practice/src/main/java/com/ohgiraffers/dockertestpractice/Chap00DockerTestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.dockertestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap00DockerTestPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap00DockerTestPracticeApplication.class, args);
+	}
+
+}
---
+++ chap00-docker-test-practice/src/test/java/com/ohgiraffers/dockertestpractice/Chap00DockerTestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.dockertestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap00DockerTestPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
